6a63ee1d88bd84cea0bb3976525bf6d6571d2309
EDERER Thomas <thomas.ederer@frequentis.com>
2026-02-09 10:15:01 +0000

    fixed file format and added check for Node.js

--- start.sh
+++ start.sh
@@ -3,6 +3,13 @@
 scriptDir=$(cd $(dirname $0) && pwd)
 cd $scriptDir
 
+node --version > /dev/null 2&>1
+exitCode=$?
+if [ $exitCode -ne 0 ]; then
+   echo "Please install Node.js before executing this script."
+   exit 1
+fi
+
 if [ ! -d ./node_modules ]; then
     echo "No folder \"node_modules\" found -> executing \"npm install\" to install dependencies ..."
     npm install
